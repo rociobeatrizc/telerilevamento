2198d5702e74d07f16d396a930d5fa5962c8be49
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2022-03-19 17:23:31 +0100

    Update R_code_sr.r

--- R_code_sr.r
+++ R_code_sr.r
@@ -1,18 +1,18 @@
-# Questo è il primo script che useremo a lezione
+# First script
 
 # install.packages("raster")
 library(raster)
 
-# Settaggio cartella di lavoro
+# Setting work directory
 setwd("C:/lab")
 
 # import
 l2011 <- brick("p224r63_2011.grd") 
 l2011
 
-# plot generico in uno spazio xy
+# plot in generic space xy
 plot(l2011)
 
 cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
-# https://www.r-graph-gallery.com/42-colors-names.html  Colori su R
+# https://www.r-graph-gallery.com/42-colors-names.html  Colors 
 plot(l2011, col = cl) 
