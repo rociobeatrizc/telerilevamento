09d9c1a34cc1008e7d0aa7adfd6bb6e426a62d46
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2022-06-03 17:34:58 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,11 +1,13 @@
-# Classification 
+# Classification: from raster data to classes. 
 # Import some code already done
 setwd("C:/lab") 
 source("r_code.txt")
 
 # Satellitar data
 library(raster)
-install.packages("RStoolbox")
+
+# unsuperClass
+install.packages("RStoolbox") 
 library(RStoolbox)
 
 # Solar Orbiter 
@@ -14,6 +16,8 @@ so
 
 plotRGB(so, 1,2,3, stretch="lin")
 plotRGB(so, 1,2,3, stretch="hist")
+
+# We set 3 classes: pixels will be divided into these. 
 soc <- unsuperClass(so, nClasses=3)
 soc
 plot(soc$map)
@@ -24,13 +28,15 @@ dev.off()
 # When John Wesley Powell led an expedition down the Colorado River and through the Grand Canyon in 1869, he was confronted with a daunting landscape. 
 # At its highest point, the serpentine gorge plunged 1,829 meters (6,000 feet) from rim to river bottom, making it one of the deepest canyons in the United States. 
 # In just 6 million years, water had carved through rock layers that collectively represented more than 2 billion years of geological history, nearly half of the time Earth has existed.
-# It is possible to associate a pixel which is still not classificated to the nearest spectral class.  
+
+# It is possible to associate a pixel which is still not classificated to the nearest spectral class. 
+# Import satellitar image already processed. 
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg") 
 gc
 plotRGB(gc, r=1, g=2, b=3, stretch="hist") 
-# Satellitar image already processed 
 
-gcclass2 <- unsuperClass(gc, nClasses = 2 ) #non supervisionata, numero di campioni (samples)
+# Two classes. 
+gcclass2 <- unsuperClass(gc, nClasses = 2 ) )
 gcclass2
 # Classification gives us an object that includes the model, output map, statistics. 
 
