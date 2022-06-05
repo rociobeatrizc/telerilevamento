d0f7e244178399e3d460e3997943da634094d12a
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2022-06-05 14:25:10 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -11,7 +11,7 @@ install.packages("viridis")
 library(raster)    # Lettura e analisi di dati spaziali: funzioni base. 
 library(RStoolbox) # Strumenti per processare e analizzare immagini telerilevate: da qui proviene la funzione focal. 
 library(ggplot2)   # Per la produzione di grafici a partire da un set di dati.
-library(patchwork) # Permette di creare multiframe con i grafici generati da ggplot. 
+library(patchwork) # Permette di creare MultiFrame con i grafici generati da ggplot. 
 library(viridis)   # Per incrementare la leggibilità. 
 
 # Impostazione Working Directory
@@ -27,55 +27,63 @@ sen <- brick("sentinel.png")
 ggRGB(sen, 1, 2, 3) 
 dev.off()
 
+# Assegno ad una variabile. 
 g1 <- ggRGB(sen, 1, 2, 3)
 
-# Si vede bene la roccia viola e la neve, mentre la vegetazione è verde (contrasto fra boschi e praterie)
-# Nel verde si monta l'infrarosso. 
+# Nel verde si monta la banda del NIR: si vedono bene la roccia viola e la neve, mentre la vegetazione è verde (contrasto fra boschi e praterie).  
+ggRGB(sen, 2, 1, 3)
+
+# Assegno ad una seconda variabile per creare un MultiFrame. 
 g2 <- ggRGB(sen, 2, 1, 3)
-# Patchwork 
+
+# Patchwork: immagini una di fianco all'altra con + , una sopra l'altra con /
+g1 + g2
 g1 / g2 
 
-# Come si misura la variabilità?
+# Come misura della variabilità del paesaggio si sceglie la deviazione standard, che esprime la dispersione dei dati intorno alla media. 
 # Si fa passare su uno strato una finestra mobile, che si sposta lungo tutta l'immagine (ciclo FOR). 
 # Fa un calcolo qualsiasi sui valori di riflettanza all'interno della finestra (3x3) e riporta un valore nel pixel centrale, che è posto al centro dell'immagine originale.
-# Intorno alla media c'è una curva di valori possibili: la deviazione standard è la misura degli scarti dalla media, al quadrato si chiama varianza ed è quella che usiamo. 
-# La varianza quindi misura la variabilità intorno alla media. 
 # Focal su singola variabile. Si sceglie un layer con informazioni (NIR), un indice spettrale...
 
-# Associamo ad una variabile l'Infrarosso: buona banda per fare questo calcolo. 
+# Occorre scegliere uno strato rappresentativo: il layer del NIR è una buona banda sulla quale calcolare la deviazione standard. 
+# Si associa la banda ad una variabile. 
 nir <- sen[[1]]
 
-# Funzione Focal (RStoolbox)
-# Variabilità su NIR, si definisce la matrice, la funzione è la deviazione standard, cioè il calcolo che scegliamo per stabilire la variabilità)  
+# Funzione Focal (RStoolbox).
+# Sullo strato scelto, focal fa passare grazie ad un ciclo FOR una finestra mobile, cioè una matrice le cui dimensioni vanno specificate. 
+# Sui valori di riflettanza all'interno della matrice, focal calcola la deviazione standard (anche questa da definire) e riporta un valore nel pixel centrale della finestra.
+# Il procedimento fino a ricoprire di pixel ricalcolati da focal tutta l'immagine originale. 
+# Matrice 3x3, funzione deviazione standard. 
 sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
 
-clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #
+# Plot
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) 
 plot(sd3, col=clsd)
 # Luoghi a bassa variabilità: roccia compatta senza crepacci (zone blu), acqua
 # Luoghi ad alta variabilità: bordi dei boschi (rosa), crepacci (rossi). 
 
-# ggplot apre il plot vuoto
-# Si aggiunge una geometria raster, si fornisce l'immagine da plottare
-# Abbiamo a che fare con un raster quindi si specifica mapping per le aest. 
+# ggplot() apre il plot vuoto, a cui si aggiunge una geometria raster.
+# Mapping per le aestethics 
 # Il colore corrisponde allo strato che abbiamo calcolato, ovvero "layer"
 ggplot() + 
   geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer)) 
 # Crepacci e limiti risultano più evidenti. 
 
 # Viridis: https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
-# Alcuni colori non possono essere visualizzati da persone con difetti visivi. 
+# Alcuni colori non possono essere visualizzati da persone con difetti visivi (es. daltonismo): viridis crea mappe di colori accessibili a tutti. 
 # Default: viridis. 
-viridis3 <- ggplot() +
+ggplot() +
   geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) +
-  scale_fill_viridis(option = "viridis") +
+  scale_fill_viridis() +
   ggtitle("Standard Deviation 3x3 by Viridis")
 
-# Visualizzazione della misura di variabilità: Inferno azzera la variabilità minima. 
+# Visualizzazione della misura di variabilità: inferno azzera la variabilità minima. 
 inferno <- ggplot() +
   geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis(option = "inferno") +
   ggtitle("Standard Deviation by Inferno")
 
+# Altro esempio: magma. 
 magma <- ggplot() +
   geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis(option = "magma") +
@@ -83,13 +91,20 @@ magma <- ggplot() +
 
 dev.off()
 
-# Posso cambiare le dimensioni della matrice, ad esempio 7x7 (meno dettaglio)
+# Posso cambiare le dimensioni della matrice, ad esempio 7x7. 
 sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
 
+# Il risultato è meno dettagliato. 
 ggplot() +
   geom_raster(sd7, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis(option = "viridis") +
   ggtitle("Standard Deviation 7x7 by Viridis")
+
+# Immagini a confronto. 
+viridis3 <- ggplot() +
+  geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) +
+  scale_fill_viridis() +
+  ggtitle("Standard Deviation 3x3 by Viridis")
 
 viridis7 <- ggplot() +
   geom_raster(sd7, mapping =aes(x=x, y=y, fill=layer)) +
