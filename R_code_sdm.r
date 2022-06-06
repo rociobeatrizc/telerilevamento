6ff6b06206fb86ac4f434a4f94e804279c4786fb
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2022-06-06 12:28:33 +0200

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -1,90 +1,96 @@
-# Species Distribution Modelling
+# Species Distribution Modelling.
+# La modellizzazione della distribuzione di specie ha due scopi: prevedere la distribuzione di una specie nello spazio e capire i parametri che la governano. 
+# Un modello prende i dati a rilevati a terra sulla presenza/assenza di una specie. 
+# Utilizzando i predittori (variabili ambientali), il modello stima dove non è stato fatto il campionamento qual è la probabilità di trovare quella specie in quel luogo.
+# Si passa così da una mappa di punti ad una mappa continua di probabilità, dove i pixel hanno un valore compreso fra 0 e 1. 
 
+# Pacchetto necessario per creare il modello. 
 install.packages("sdm")
-library(sdm)
-library(raster) # Predictors
-library(rgdal) # Species 
 
-# ggplot visualization 
-# library(ggplot2)
-# library(patchwork)
-# library(viridis)
- 
-# In sdm we can already find a shapefile with the specie: there is no need to set the working directory.
+library(sdm)    # Specie e predittori (variabili ambientali). 
+library(raster) # Lettura e analisi di dati spaziali. 
+library(rgdal)  # Geospatial Data Abstraction Library.   
+
+# Non si usa la WD, ma un file incluso nel pacchetto sdm che va caricato con system file (pacchetto raster).
 file <- system.file("external/species.shp", package="sdm") 
 
 # Path
 file
 
-# Spatial Point Dataframe (not raster file) with the occurrences of the specie (presence/absence)
+# Non è un raster, ma uno Spatial Point Dataframe (punti nello spazio): nel file c'è la presenza/assenza della specie (Occurence). 
+# Shapefile legge i file vettoriali
 species <- shapefile(file)
 species
 
-# Presence/absence  
+# Plot della presenza di una specie nello spazio.  
 plot(species, pch=19)
 
-# Presence 1, absence 0. 
+# Tabella di presenza/assenza. 
 species$Occurrence
+# 1 presenza, 0 assenza. 
 
-# Blue points: presence. Red points: absence. 
+# Plot  degli individui che hanno occorrenza uguale ad 1, cioè sono presenti, in blu. 
+# Quelli assenti in rosso.  
 occ <- species$Occurrence
 plot(species[occ == 1,], col='blue', pch=19)
 points(species[occ == 0,], col='red', pch=19)
 dev.off()
 
-# Predictors are parameters that can be useful to predict where the specie is. 
-# Path: system.file 
+# Predictors o Predittori: ci aiutano a prevedere dove sarà la specie. 
+# Si ricava il path .
 path <- system.file("external", package="sdm") 
 
-# List of files (full.names keeps the original path).
+# Si prendono tutti i file con estensione asc e si crea una lista: full.names conserva il percorso.
 lst <- list.files(path=path, pattern='asc$', full.names = T) 
 lst
 
-# It is not necessary to use brick function to import the list: is already inside the package. 
-# Instead of bands, we have geographic data (proxy). 
+# Essendo già dentro al pacchetto, non abbiamo bisogno della funzione brick per importare la lista. 
+# Al posto delle bande abbiamo 4 variabili, i nostri predittori: quota, precipitazioni, temperatura, vegetazione.  
 preds <- stack(lst)
 cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
 plot(preds, col=cl)
 
-# Predictors into a variable
+# Assegniamo ogni predittore ad una variabile 
 elev <- preds$elevation
 prec <- preds$precipitation
 temp <- preds$temperature
 vege <- preds$vegetation
-
-# Exploring the specie's distribution with respect to each variable. 
-# Elevation
+ 
 plot(elev, col=cl)
 points(species[occ == 1,], col='black', pch=19)
+# Specie a cui non piace stare a quote elevate.
 
-# Temperature 
 plot(temp, col=cl)
 points(species[occ == 1,], col='black', pch=19)
+# Specie che non ama il freddo. 
 
-# Precipitation 
 plot(prec, col=cl)
 points(species[occ == 1,], col='black', pch=19)
-
-# Vegetation 
+# Si trova in luoghi con medio alta quantità di precipitazioni. 
+  
 plot(vege, col=cl)
 points(species[occ == 1,], col='black', pch=19)
+# Non si trova dove la vegetazione scarseggia.
 
 dev.off()
 
-# Das Model: species and predictors as data. 
+# Das Model: funzioni all'interno del pacchetto sdm. Specie e predittori sono i dati. 
+# All'interno del pacchetto sdm si trova sdmData, che costruisce il modello. 
 datasdm <- sdmData(train=species, predictors=preds)
+# I dati di train sono le specie, i predittori le variabili ambientali. 
 
-# Linear model. 
-# Y (output): occurrence. 
-# X (input): predictors. 
+# Modello lineare. 
 m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=datasdm, methods = "glm")
+# In input (x): dati ambientali (variabili indipendenti).
+# In output (y): Occurence. 
+# Metodo: glm, Generalized Linear Models. 
 
-# Prediction Map: raster file. 
+# Mappa finale di previsione: oggetto raster con la probabilità di trovare la specie nello spazio.    
 p1 <- predict(m1, newdata=preds)
 plot(p1, col=cl)
 points(species[species$Occurrence == 1,], pch=16)
 
-# MultiFrame with predictors.
+# MultiFrame con la mappa di previsione e le variabili.
 par(mfrow=c(2,3))
 plot(p1, col=cl)
 plot(elev, col=cl)
@@ -92,6 +98,6 @@ plot(prec, col=cl)
 plot(temp, col=cl)
 plot(vege, col=cl)
 
-# More easily: 
+# In alternativa
 final <- stack(preds, p1)
 plot(final, col=cl)
