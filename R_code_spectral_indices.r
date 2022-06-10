ee185a13fbe19f6475eaaa9b1d9641480d8f73a3
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2022-06-10 17:26:45 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,25 +1,18 @@
 # Indici spettrali. 
 # Si possono usare le bande per visualizzare le immagini satellitari in modi differenti ed evidenziare alcune componenti dell'ecosistema rispetto ad altre. 
 # L'NDVI (Normalized Difference Vegetation Index) è un indice spettrale di vegetazione basato sulla differenza fra le bande del NIR e del RED. 
-# In un pixel con un periodo di picco di biomassa, in una scala di riflettanza (range da 0 a 100) il valore nel NIR sarà alto: lo stesso pixel ha una riflettanza nel rosso bassa. 
-# DVI = NIR - RED
-# In una pianta che soffre il mesofillo foliare collassa variando gli angoli di riflessione. 
-# Si abbassa la riflettanza nel NIR, si innalza quella nel rosso (non viene assorbito a sufficienza). 
 
-# Libreria per tutti i dati in formato immagine. 
+# Pacchetti da installare. 
+install.packages("rgdal")     # Dati spaziali
+install.packages("RStoolbox") # Processing di immagini telerilevate.
+install.packages("rasterdiv") # Indici di diversità
+
 library(raster)
-
-# Libreria per usare dati spaziali. 
-install.packages("rgdal")
 library(rgdal)
-
-# Toolbox for remote sensing image processing and analysis such as calculating spectral indices, principal component transformation, unsupervised and supervised classification or fractional cover analyses
-install.packages("RStoolbox") 
 library(RStoolbox)
-
-install.packages("rasterdiv")
 library(rasterdiv)
 
+# Set WD
 setwd("C:/lab")
 
 # Earth Observatory NASA: Rio Peixoto, dati già analizzati. Si importano tutte le bande insieme. 
@@ -57,8 +50,8 @@ par(mfrow= c(2,1))
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
-# L'acqua assorbe molto nel NIR, infatti l'acqua tende al nero nella seconda immagine. Nell'immagine sopra è visibile
-# il sedimento del fiume, molto simile alla colorazione del suolo nudo. 
+# L'acqua assorbe molto nel NIR, infatti l'acqua tende al nero nella seconda immagine.
+# Nell'immagine sopra è visibile il sedimento del fiume, simile alla colorazione del suolo nudo. 
 # Massimo DVI= 255 (NIR-RED)
 
 dvi1992 = l1992[[1]] - l1992[[2]]
@@ -74,10 +67,10 @@ plot(dvi2006, col=cl)
 par(mfrow=c(2,1)) 
 plot(dvi1992, col=cl)
 plot(dvi2006, col=cl)
-# Giallo = male, molta deforestazione. Per distruggere un habitat basta frammentarlo a sufficienza 
-# Popolazioni non siano più a contatto (individui stessa specie)
+# Giallo = molta deforestazione. Per distruggere un habitat basta frammentarlo a sufficienza .
+# In questo modo infatti le popolazioni non vengono più a contatto. 
 
-# Si possono usare i nomi
+# Si possono usare i nomi per selezionare. 
 # nomi: defor2_.1, defor2_.2, defor2_.3
 
 # Per confrontarle si può fare la differenza 
@@ -87,7 +80,8 @@ dvi_diff
 cld <- colorRampPalette(c('blue','white','red'))(100)
 plot(dvi_diff, col=cld)
  
-# NDVI: differenza normalizzata. Si divide la differenza per la somma delle due bande: in questo modo si possono fare confronti. 
+# NDVI: differenza normalizzata. 
+# Si divide la differenza per la somma delle due bande: in questo modo si possono fare confronti. 
 ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
 ndvi1992
 plot(ndvi1992, col=cl)
