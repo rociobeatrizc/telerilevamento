3236ecd828d921bed28ed35bc387be1af721f388
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2022-06-11 10:22:43 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -1,27 +1,37 @@
-# Time Series Analysis of Greenland LST data from Copernicus. 
+# Time Series Analysis
+# Come cambia una regione nel tempo visualizzando più immagini satellitari: si prende in considerazione la Groenlandia.  
+
+# Pacchetto da installare: miglior visualizzazione e interazione con i dati raster. 
 install.packages("rasterVis")
-library(raster)
-library(rasterVis) 
-library(rgdal)
 
-# LST stands for Land Surface Temperature. 
-# Setting work directory
-setwd("C:/lab/greenland") 
+# Librerie. 
+library(raster)    # Lettura e manipolazionedi dati satellitari. 
+library(rasterVis) # Visualizzazione raster. 
+library(rgdal)     # GDAL (Geospatial Data Abstraction Library) per R. 
 
-# One satellitar image is made of channels: brick function loads the whole package of bands, but in this case we have four separated data. 
-# raster function imports a single layer, one by one. 
+# Come parametro da valutare nel corso del tempo si usa il Land Surface Temperature (LST).
+# Misura la temperatura della terra al suolo. 
+
+# Cartella di lavoro. 
+setwd("C:/lab/greenland")
+
+# Con la funzione Brick si carica l'intero blocco di bande che compongono un'immmagine in R. 
+# Con la funzione Raster si importa un singolo layer: abbiamo 4 dati diversi. 
 lst2000 <- raster("lst_2000.tif")
 lst2005 <- raster("lst_2005.tif")
 lst2010 <- raster("lst_2010.tif")
 lst2015 <- raster("lst_2015.tif")
 
 lst2000
+# Risoluzione Radiometrica: bit a disposizione. 
 # 16 bit. 
+2^16
 
+# Plot di una sola immagine. 
 plot(lst2015)
 dev.off()
 
-# MultiFrame 
+# Plot del MultiFrame 
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 par(mfrow=c(4,1)) 
 plot(lst2000, col=cl)
@@ -29,21 +39,22 @@ plot(lst2005, col=cl)
 plot(lst2010, col=cl)
 plot(lst2015, col=cl)
 
-# Simplifying, lapply fuction applies raster function to each image. 
-# First, a list is needed. 
+# list.files, lapply, stack. 
+# Al posto di importare gli oggetti uno ad uno, si crea una lista  estraendo tutti i file che hanno in comune la stringa "lst".
+# Ad rlist si applica la funzione raster con lapply. 
 rlist <- list.files(pattern="lst")
 import <- lapply(rlist, raster)
 import
 
-# stack function puts layers together in a single file, like a satellitar frame. 
+# Con la funzione stack si inseriscono i layer in un unico file tipo immagine satellitare. 
 tgr <- stack(import)
-tgr
-dev.off()
+
+# Per plottare una sola immagine devo richiamarla con la posizione o con il suo nome. 
 plot(tgr[[1]], col=cl)
 
-# or
+# Oppure
 plot(tgr$lst_2000, col=cl)
 
-# RGB.
+# Possiamo fare un RGB basandoci sui Layer
 plotRGB(tgr, r=1, g=2, b=3, stretch="lin")
-# Black part is in common. 
+# Parte nera: in comune. 
